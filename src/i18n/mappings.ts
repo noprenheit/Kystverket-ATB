6251996a992c52a828a28887e06431c802595e9e
Mehmet Selcuk Öner <selcukoner92@gmail.com>
2025-05-05 04:45:37 +0200

    Common API returns mapped to translate later

---
+++ src/i18n/mappings.ts
@@ -0,0 +1,40 @@
+// Weather conditions
+export const conditionMappings: Record<string, string> = {
+  // Common weather conditions
+  'Clear': 'weather.condition.clear',
+  'Sunny': 'weather.condition.sunny',
+  'Partly cloudy': 'weather.condition.partly_cloudy',
+  'Cloudy': 'weather.condition.cloudy', 
+  'Overcast': 'weather.condition.overcast',
+  'Mist': 'weather.condition.mist',
+  'Fog': 'weather.condition.fog',
+  'Light rain': 'weather.condition.light_rain',
+  'Moderate rain': 'weather.condition.moderate_rain',
+  'Heavy rain': 'weather.condition.heavy_rain',
+  'Light snow': 'weather.condition.light_snow',
+  'Moderate snow': 'weather.condition.moderate_snow', 
+  'Heavy snow': 'weather.condition.heavy_snow',
+  'Thunderstorm': 'weather.condition.thunderstorm',
+  'Sleet': 'weather.condition.sleet',
+  'Drizzle': 'weather.condition.drizzle'
+};
+
+// Wind directions
+export const windDirectionMappings: Record<string, string> = {
+  'N': 'weather.wind_direction.n',
+  'NNE': 'weather.wind_direction.nne',
+  'NE': 'weather.wind_direction.ne',
+  'ENE': 'weather.wind_direction.ene',
+  'E': 'weather.wind_direction.e',
+  'ESE': 'weather.wind_direction.ese',
+  'SE': 'weather.wind_direction.se',
+  'SSE': 'weather.wind_direction.sse',
+  'S': 'weather.wind_direction.s',
+  'SSW': 'weather.wind_direction.ssw',
+  'SW': 'weather.wind_direction.sw',
+  'WSW': 'weather.wind_direction.wsw',
+  'W': 'weather.wind_direction.w',
+  'WNW': 'weather.wind_direction.wnw',
+  'NW': 'weather.wind_direction.nw',
+  'NNW': 'weather.wind_direction.nnw'
+}; 
