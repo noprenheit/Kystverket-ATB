e6b526e7e75a933f76b499271325c86640384591
Mehmet Selcuk Öner <selcukoner92@gmail.com>
2025-05-05 04:51:04 +0200

    expo localization plugin added

--- app.config.js
+++ app.config.js
@@ -3,6 +3,9 @@ import 'dotenv/config';
 export default ({ config }) => {
   return {
     ...config,
+    plugins: [
+      "expo-localization"
+    ],
     extra: {
       weatherApiKey: process.env.WEATHER_API_KEY,
     },
